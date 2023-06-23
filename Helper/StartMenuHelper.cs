2990bf7856ddfe59da37153d0d9e506f044c9368
Raphael Vogel <ra.vo@hotmail.com>
2023-06-23 16:01:44 +0200

    No need for .NET COM Interop or Windows Script Host

--- Helper/StartMenuHelper.cs
+++ Helper/StartMenuHelper.cs
@@ -1,4 +1,4 @@
-﻿using IWshRuntimeLibrary;
+﻿using WindowsShortcutFactory;
 
 namespace Helper
 {
@@ -6,21 +6,21 @@ namespace Helper
     {
         public static void CreateShortcutToDesktop(string path, string linkName, string arguments = "", string description = "")
         {
-            object shDesktop = "Desktop";
-            WshShell shell = new();
-            string shortcutAddress = Path.Combine((string)shell.SpecialFolders.Item(ref shDesktop), linkName + ".lnk");
+            using WindowsShortcut _windowsShortcut = new()
+            {
+                Path = path,
+                Arguments = arguments,
+                WorkingDirectory = Path.GetDirectoryName(path),
+                IconLocation = path,
+                Description = description
+            };
 
-            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
-            shortcut.Description = description;
-            shortcut.TargetPath = path;
-            shortcut.Arguments = arguments;
-            shortcut.WorkingDirectory = Path.GetDirectoryName(path);
-            shortcut.Save();
+            _windowsShortcut.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), linkName + ".lnk"));
         }
 
         public static bool DoesTheShortCutExist(string path, string shortcutName)
         {
-            if (System.IO.File.Exists(Path.Combine(path, shortcutName + ".lnk")))
+            if (File.Exists(Path.Combine(path, shortcutName + ".lnk")))
             {
                 return true;
             }
@@ -32,15 +32,16 @@ namespace Helper
 
         public static void CreateShortcutToStartMenu(string path, string linkName, string optionalSubPath = "", string arguments = "", string description = "")
         {
-            WshShell shell = new();
-            string shortcutAddress = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu), optionalSubPath, linkName + ".lnk");
+            using WindowsShortcut _windowsShortcut = new()
+            {
+                Path = path,
+                Arguments = arguments,
+                WorkingDirectory = Path.GetDirectoryName(path),
+                IconLocation = path,
+                Description = description
+            };
 
-            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
-            shortcut.Description = description;
-            shortcut.TargetPath = path;
-            shortcut.Arguments = arguments;
-            shortcut.WorkingDirectory = Path.GetDirectoryName(path);
-            shortcut.Save();
+            _windowsShortcut.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu), linkName + ".lnk"));
         }
     }
 }
