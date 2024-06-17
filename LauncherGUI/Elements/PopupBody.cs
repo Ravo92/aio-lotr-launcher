d541ccae82a3f1fd6a650ca0a81d8c4c7e58ac5e
Gazdag Marcell <40302952+MarcellVokk@users.noreply.github.com>
2024-06-17 09:08:45 +0200

    New Install dialog

---
+++ LauncherGUI/Elements/PopupBody.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace LauncherGUI.Elements
+{
+    public abstract class PopupBody : UserControl
+    {
+        public Action<string[]>? OnSubmited;
+        public Action? ClosePopup;
+
+        public void Submit(params string[] data)
+        {
+            OnSubmited?.Invoke(data);
+            ClosePopup?.Invoke();
+        }
+
+        public void Dismiss() => ClosePopup?.Invoke();
+    }
+}
