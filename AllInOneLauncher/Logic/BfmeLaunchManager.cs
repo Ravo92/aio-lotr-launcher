c1f4e4359ebd092bc8c7e8a86e9ab6994ef39ef2
Gazdag Marcell <40302952+MarcellVokk@users.noreply.github.com>
2024-08-12 22:19:51 +0200

    If a mod is active, launch the game with the -mod switch.

--- AllInOneLauncher/Logic/BfmeLaunchManager.cs
+++ AllInOneLauncher/Logic/BfmeLaunchManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using AllInOneLauncher.Data;
 using BfmeFoundationProject.BfmeRegistryManagement;
 using BfmeFoundationProject.BfmeRegistryManagement.Data;
+using BfmeFoundationProject.WorkshopKit.Logic;
 
 namespace AllInOneLauncher.Logic
 {
@@ -13,15 +14,14 @@ namespace AllInOneLauncher.Logic
         {
             BfmeRegistryManager.EnsureDefaults((int)game);
 
-            using Process? gameProcess = Process.Start(new ProcessStartInfo()
-            {
-                WorkingDirectory = BfmeRegistryManager.GetKeyValue((int)game, BfmeRegistryKey.InstallPath),
-                FileName = Path.Combine(BfmeRegistryManager.GetKeyValue((int)game, BfmeRegistryKey.InstallPath), BfmeDefaults.DefaultGameExecutableNames[(int)game]),
-                Arguments = windowed ? $"-win -xres {SystemDisplayManager.GetPrimaryScreenResolution().Width - 100} -yres {SystemDisplayManager.GetPrimaryScreenResolution().Height - 100}" : "",
-            });
+            ProcessStartInfo startInfo = new();
+            startInfo.WorkingDirectory = BfmeRegistryManager.GetKeyValue((int)game, BfmeRegistryKey.InstallPath);
+            startInfo.FileName = Path.Combine(BfmeRegistryManager.GetKeyValue((int)game, BfmeRegistryKey.InstallPath), BfmeDefaults.DefaultGameExecutableNames[(int)game]);
+            if (windowed) startInfo.ArgumentList.Add($"-win -xres {SystemDisplayManager.GetPrimaryScreenResolution().Width - 100} -yres {SystemDisplayManager.GetPrimaryScreenResolution().Height - 100}");
+            if (BfmeWorkshopSyncManager.GetActiveModPath((int)game) != null) startInfo.ArgumentList.Add($"-mod {BfmeWorkshopSyncManager.GetActiveModPath((int)game)}");
 
-            if (gameProcess == null)
-                return;
+            using Process? gameProcess = Process.Start(startInfo);
+            if (gameProcess == null) return;
 
             SystemWindowManager.UpdateWindow(gameProcess.MainWindowHandle, 0, 0, SystemDisplayManager.GetPrimaryScreenResolution().Width, SystemDisplayManager.GetPrimaryScreenResolution().Height, windowed);
             SystemInputManager.ConfineCursor(0, 0, SystemDisplayManager.GetPrimaryScreenResolution().Width, SystemDisplayManager.GetPrimaryScreenResolution().Height, windowed);
